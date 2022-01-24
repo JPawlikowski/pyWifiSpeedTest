db521c0fded4d04d79c00a290e11f2d5a96eeb2c
Jakub Pawlikowski <pawlikoj@mcmaster.ca>
2022-01-23 22:41:04 -0500

    uploads and ping graphs

--- analysis_runner.py
+++ analysis_runner.py
@@ -3,6 +3,7 @@ import os
 #import sys
 #sys.path.append('/usr/local/Cellar/python@3.9/3.9.8/bin/')
 
+from datetime import datetime
 import numpy
 import pandas as pd
 
@@ -27,13 +28,40 @@ for filename in os.listdir(results_path):
 for val in downloads:
 	print(val)
 
+#NOTE
+#Using 'x' later on for length of UPLOADS and PINGS lists
+#Fine as long as all entry data has records for 3 fields (len(downloads)==len(uploads)==len(pings)
 x = list(range(0,len(downloads)))
-print(x)
+#print(x)
+
+dt = datetime.now()
+current_time = dt.strftime("%Y%m%d%H%M")
+
+#DOWNLOADS
+current_file_downloads = str("./graphs/downloads_plot_"+current_time+".png")
 
 plt.plot(x, downloads)
 
-plt.savefig('./graphs/file1.png', dpi=None, facecolor='w', edgecolor='w', orientation='portrait', papertype=None, format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)
+plt.savefig(current_file_downloads, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)
 
-plt.show()
+#plt.show()
 
+#UPLAODS
+current_file_uploads = str("./graphs/uploads_plot_"+current_time+".png")
+
+plt.plot(x, uploads)
+
+plt.savefig(current_file_uploads, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)
+
+#plt.show()
+
+#PINGS
+
+current_file_pings = str("./graphs/ping_plot_"+current_time+".png")
+
+plt.plot(x, pings)
+
+plt.savefig(current_file_pings, dpi=None, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches=None, pad_inches=0.1, frameon=None, metadata=None)
+
+#plt.show()
 
